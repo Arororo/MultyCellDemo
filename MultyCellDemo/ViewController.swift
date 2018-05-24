6d5039326e25cc15c8e218728be2b17edbc3f966
Dmitry Babenko <dmitry.babenko@aequilibrium.ca>
2018-05-24 14:41:36 -0700

    Initial Commit

---
+++ MultyCellDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MultyCellDemo
+//
+//  Created by Dmitry Babenko on 2018-05-24.
+//  Copyright © 2018 Dmitry Babenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
